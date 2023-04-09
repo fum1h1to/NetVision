0557330e04e5949c03a76e02c55f728a004deea8
Fumihito Sudo <75571344+fum1h1to@users.noreply.github.com>
2023-04-09 19:19:11 +0900

    libpcapを含めたい

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 rm -rf ./build
 mkdir -p ./build
 
-docker build -f ./Dockerfile.build -t fum1h1to/net-vision-builder:build .
+docker build --no-cache -f ./Dockerfile.build -t fum1h1to/net-vision-builder:build .
 docker create --name netvision-builder fum1h1to/net-vision-builder:build
 docker cp netvision-builder:/build ./
 docker rm netvision-builder
